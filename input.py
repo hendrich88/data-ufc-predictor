f9960e7a02a9541a423641105c9ddbc8233eee57
hendrich88 <hendrich88@gmail.com>
2026-01-27 19:51:32 +0100

    Update input.py

--- input.py
+++ input.py
@@ -2,18 +2,21 @@ event_date = "2026-01-31"
 event = "UFC 325: Volkanovski vs. Lopes 2"
 
 # Vstupní seznamy zápasníků
-event_fighters1 = ["Justin Gaethje","Sean O'Malley","Waldo Cortes Acosta","Natalia Silva","Arnold Allen","Umar Nurmagomedov",
-                   "Ateba Gautier","Nikita Krylov","Alex Perez","Michael Johnson","Ricky Turcios"] 
+event_fighters1 = ["Alexander Volkanovski","Dan Hooker","Rafael Fiziev","Tai Tuivasa","Quillan Salkilld","Junior Tafa", "Cam Rowston",
+                   "Jacob Malkoun","Jonathan Micallef","Kaan Ofli","Sangwook Kim","Keiichiro Nakamura","Sulangrangbo", "Aaron Tau"] 
 
-event_fighters2 = ["Paddy Pimblett","Song Yadong","Derrick Lewis","Rose Namajunas","Jean Silva","Deiveson Figueiredo",
-                   "Andrey Pulyaev","Modestas Bukauskas","Charles Johnson","Alexander Hernandez","Cameron Smotherman"]   
+event_fighters2 = ["Diego Lopes","Benoit Saint Denis","Mauricio Ruffy","Tallison Teixeira","Jamie Mullarkey","Billy Elekana", "Cody Brundage",
+                   "Torrez Finney","Oban Elliott","Yizha","Dom Mar Fan","Sebastian Szalay","Lawrence Lui", "Namsrai Batbayar"] 
 
 # Sázkové kurzy
-odds_fighters1 = [2.69, 1.49, 1.29, 1.22, 2.99, 1.06, 1.10, 2.16, 2.51, 2.19, 2.41] 
+odds_fighters1 = [ 1.63, 3.17, 1.92, 3.13, 1.08, 2.79, 1.25,
+                   1.65, 1.69, 2.57, 2.30, 1.99, 1.37, 2.03] 
 
-odds_fighters2 = [1.41, 2.50, 3.28, 3.74, 1.35, 7.33, 5.75, 1.63, 1.46, 1.61, 1.49]
+odds_fighters2 = [ 2.19, 1.29, 1.80, 1.30, 6.26, 1.37, 3.49,
+                   2.13, 2.05, 1.43, 1.55, 1.74, 2.79, 1.71] 
 
-hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+hit = [-1, -1, -1, -1, -1, -1, -1,
+       -1, -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
