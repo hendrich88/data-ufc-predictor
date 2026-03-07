f43c91fc0a6b655b80fbf390a9bc54894327e80e
hendrich88 <hendrich88@gmail.com>
2026-03-07 22:42:57 +0100

    Update input.py

--- input.py
+++ input.py
@@ -3,16 +3,16 @@ edge = -666666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-01-31"
-event = "UFC 325: Volkanovski vs. Lopes 2"
+event_date = "2026-02-07"
+event = "UFC Fight Night: Bautista vs. Oliveira"
 
-event_fighters1 = ["Alexander Volkanovski", "Benoit Saint Denis", "Mauricio Ruffy", "Tallison Teixeira", "Quillan Salkilld", "Cam Rowston", "Yizha", "Dom Mar Fan", "Keiichiro Nakamura", "Sulangrangbo", "Namsrai Batbayar"]
-event_fighters2 = ["Diego Lopes", "Dan Hooker", "Rafael Fiziev", "Tai Tuivasa", "Jamie Mullarkey", "Cody Brundage", "Kaan Ofli", "Sangwook Kim", "Sebastian Szalay", "Lawrence Lui", "Aaron Tau"]  
+event_fighters1 = ["Vinicius Oliveira", "Jailton Almeida", "Michal Oleksiejczuk", "Farid Basharat", "Julius Walker", "Ketlen Souza", "Said Nurmagomedov", "Klaudia Sygula"]
+event_fighters2 = ["Mario Bautista", "Rizvan Kuniev", "Marc-Andre Barriault", "Jean Matsumoto", "Dustin Jacoby", "Bruna Brasil", "Javid Basharat", "Priscila Cachoeira"]
 
 # Sázkové kurzy
-odds_fighters1 = [1.63, 1.29, 1.80, 1.30, 1.08, 1.25, 1.43, 1.55, 1.99, 1.37, 1.71]
-odds_fighters2 = [2.19, 3.17, 1.92, 3.13, 6.25, 3.48, 2.57, 2.30, 1.74, 2.79, 2.03]
-hit = [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, -1]
+odds_fighters1 = [2.38, 1.64, 1.19, 1.34, 2.40, 1.62, 1.93, 1.63]
+odds_fighters2 = [1.53, 2.17, 4.17, 3.07, 1.50, 2.18, 1.79, 2.16]
+hit = [0, 0, 1, 1, 0, 1, -1, 1]
 
 
 event_accuracy = 0
