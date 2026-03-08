c7abafb58fd8efa9a0f72f94f08d7e2c2ace8136
hendrich88 <hendrich88@gmail.com>
2026-03-08 23:28:24 +0100

    Update input.py

--- input.py
+++ input.py
@@ -3,16 +3,16 @@ edge = 0
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-01-24"
-event = "UFC 324: Gaethje vs. Pimblett"
+event_date = "2026-01-31"
+event = "UFC 325: Volkanovski vs. Lopes 2"
 
-event_fighters1 = ["Paddy Pimblett","Sean O'Malley","Derrick Lewis","Natalia Silva","Jean Silva","Umar Nurmagomedov","Ateba Gautier","Modestas Bukauskas","Charles Johnson"]
-event_fighters2 = ["Justin Gaethje","Song Yadong","Waldo Cortes Acosta","Rose Namajunas","Arnold Allen","Deiveson Figueiredo","Andrey Pulyaev","Nikita Krylov","Alex Perez"]
+event_fighters1 = ["Alexander Volkanovski","Benoit Saint Denis","Mauricio Ruffy","Tallison Teixeira","Quillan Salkilld","Cam Rowston","Yizha","Sangwook Kim","Sebastian Szalay","Sulangrangbo"]
+event_fighters2 = ["Diego Lopes","Dan Hooker","Rafael Fiziev","Tai Tuivasa","Jamie Mullarkey","Cody Brundage","Kaan Ofli","Dom Mar Fan","Keiichiro Nakamura","Lawrence Lui"]
 
-odds_fighters1 = [1.41,1.49,3.28,1.22,1.35,1.06,1.10,1.63,1.46]
-odds_fighters2 = [2.69,2.50,1.29,3.75,2.99,7.35,5.75,2.16,2.51]
+odds_fighters1 = [1.63,1.29,1.80,1.30,1.08,1.25,1.43,2.30,1.74,1.37]
+odds_fighters2 = [2.19,3.17,1.92,3.13,6.25,3.48,2.57,1.55,1.99,2.79]
 
-hit = [0,1,0,1,1,1,1,0,0]
+hit = [1,1,1,1,1,1,0,1,1,0]
 
 event_accuracy = 0
 event_roi = 0
