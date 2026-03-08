3f7764a56a19dbd2d979f5dec3adb9076f937aaf
hendrich88 <hendrich88@gmail.com>
2026-03-08 23:51:34 +0100

    Update input.py

--- input.py
+++ input.py
@@ -3,16 +3,16 @@ edge = 0
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-02-28"
-event = "UFC Fight Night: Moreno vs. Kavanagh"
+event_date = "2026-03-07"
+event = "UFC 326: Holloway vs. Oliveira 2"
 
-event_fighters1 = ["David Martinez","King Green","Edgar Chairez","Ailin Perez","Francis Marshall"]
-event_fighters2 = ["Marlon Vera","Daniel Zellhuber","Felipe Bunes","Macy Chiasson","Erik Silva"]
+event_fighters1 = ["Max Holloway","Caio Borralho","Rob Font","Michael Johnson","Gregory Rodrigues","Xiao Long","Donte Johnson","Nyamjargal Tumendemberel","Jesus Aguilar"]
+event_fighters2 = ["Charles Oliveira","Reinier de Ridder","Raul Rosas Jr.","Drew Dober","Brunno Ferreira","Cody Garbrandt","Cody Brundage","Cody Durden","Sumudaerji"]
 
-odds_fighters1 = [1.35,4.00,1.29,1.54,1.15]
-odds_fighters2 = [4.17,1.19,3.60,2.50,2.00]
+odds_fighters1 = [1.44,1.32,2.58,1.78,1.48,1.64,1.14,1.70,2.77]
+odds_fighters2 = [2.60,3.02,1.43,1.94,2.45,2.14,4.85,2.05,1.38]
 
-hit = [1,1,1,1,1]
+hit = [0,1,0,0,1,0,1,1,0]
 
 event_accuracy = 0
 event_roi = 0
