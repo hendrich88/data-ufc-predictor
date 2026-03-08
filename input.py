b690fecad7c3f7284e2ed9824659c6a43771dd6f
hendrich88 <hendrich88@gmail.com>
2026-03-08 23:02:37 +0100

    Update input.py

--- input.py
+++ input.py
@@ -1,22 +1,18 @@
 limit_pred = 60
-edge = -666
+edge = 0
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-03-07"
-event = "UFC 326: Holloway vs. Oliveira 2"
+event_date = "2026-01-24"
+event = "UFC 324: Gaethje vs. Pimblett"
 
-# Vstupní seznamy zápasníků
-event_fighters1 = ["Max Holloway", "Caio Borralho", "Rob Font", "Drew Dober", "Gregory Rodrigues", "Cody Garbrandt",
-                   "Donte Johnson", "Cody Durden", "Sumudaerji", "JeongYeong Lee"]
-event_fighters2 = ["Charles Oliveira", "Reinier de Ridder", "Raul Rosas Jr.", "Michael Johnson", "Brunno Ferreira", "Xiao Long",
-                   "Cody Brundage", "Nyamjargal Tumendemberel", "Jesus Aguilar", "Gaston Bolanos"]
+event_fighters1 = ["Paddy Pimblett","Sean O'Malley","Derrick Lewis","Natalia Silva","Jean Silva","Umar Nurmagomedov","Ateba Gautier","Modestas Bukauskas","Charles Johnson"]
+event_fighters2 = ["Justin Gaethje","Song Yadong","Waldo Cortes Acosta","Rose Namajunas","Arnold Allen","Deiveson Figueiredo","Andrey Pulyaev","Nikita Krylov","Alex Perez"]
 
-# Sázkové kurzy
-odds_fighters1 = [1.44, 1.32, 2.58, 1.94, 1.48, 2.14, 1.14, 2.05, 1.38, 1.32]
-odds_fighters2 = [2.60, 3.02, 1.43, 1.78, 2.45, 1.64, 4.85, 1.70, 2.77, 3.00]
+odds_fighters1 = [1.41,1.49,3.28,1.22,1.35,1.06,1.10,1.63,1.46]
+odds_fighters2 = [2.69,2.50,1.29,3.75,2.99,7.35,5.75,2.16,2.51]
 
-hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+hit = [0,1,0,1,1,1,1,0,0]
 
 event_accuracy = 0
 event_roi = 0
