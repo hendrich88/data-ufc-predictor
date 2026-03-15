317c57aee84827174822d75add7673924000c859
hendrich88 <hendrich88@gmail.com>
2026-03-15 14:08:59 +0100

    Update input.py

--- input.py
+++ input.py
@@ -15,7 +15,7 @@ event_fighters2 = ["Kevin Vallejos", "Gillian Robertson", "Oumar Sy", "Jose Delg
 odds_fighters1 = [4.56, 2.44, 2.58, 3.11, 1.41, 1.46, 1.69, 2.97, 1.51, 1.63]
 odds_fighters2 = [1.16, 1.48, 1.43, 1.30, 2.64, 2.50, 2.06, 1.33, 2.38, 2.16]
 
-hit = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+hit = [1, 1,0,1,1,1,1,1,0,0]
 
 event_accuracy = 0
 event_roi = 0
