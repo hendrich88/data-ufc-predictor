771d76534c42c9606a8eb8ad89001d16fbef41dd
hendrich88 <hendrich88@gmail.com>
2026-03-25 01:05:25 +0100

    Update input.py

--- input.py
+++ input.py
@@ -6,17 +6,19 @@ min_loser_fights = 0
 event_date = "2026-03-10"
 event = "UFC Fight Night: Emmett vs. Vallejos"
 
-event_date = "2026-03-21"
-event = "UFC Fight Night: Evloev vs. Murphy"
+event_date = "2026-03-28"
+event = "UFC Fight Night: Adesanya vs. Pyfer"
 
-event_fighters1 = ["Movsar Evloev", "Michael Page", "Iwo Baraniewski", "Roman Dolidze"] 
+event_fighters1 = ["Israel Adesanya", "Alexa Grasso", "Michael Chiesa", "Mansur Abdul-Malik", "Terrance McKinney", "Casey O'Neill", "Navajo Stirling",
+                  "Ricky Simon", "Alexia Thainara"] 
 
-event_fighters2 =  ["Lerone Murphy", "Sam Patterson", "Austen Lane", "Christian Leroy Duncan"]
+event_fighters2 =  ["Joe Pyfer", "Maycee Barber", "Niko Price", "Yousri Belgaroui", "Kyle Nelson", "Gabriella Fernandes", "Bruno Lopes",
+                  "Adrian Yanez", "Bruna Brasil"] 
 
-odds_fighters1 = [1.41, 1.44, 1.19, 3.48, 1.99]
-odds_fighters2 = [2.65, 2.54, 4.06, 1.25, 1.74]
+odds_fighters1 = [1.60, 2.06, 1.20, 1.77, 1.67, 2.00, 1.21, 1.57, 1.16]
+odds_fighters2 = [2.20, 1.69, 3.98, 1.95, 2.10, 1.73, 3.84, 2.25, 4.54]
 
-hit = [-1,-1,-1,-1]
+hit = [-1,-1,-1,-1, -1,-1,-1,-1, -1]
 
 event_accuracy = 0
 event_roi = 0
