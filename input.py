643662a773ab85bad2bc3f3ffad243d576e8c239
hendrich88 <hendrich88@gmail.com>
2026-03-29 09:14:06 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,22 +3,17 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-03-10"
-event = "UFC Fight Night: Emmett vs. Vallejos"
+event_date = "2026-04-04"
+event = "UFC Fight Night: Moicano vs. Duncan"
 
-event_date = "2026-03-28"
-event = "UFC Fight Night: Adesanya vs. Pyfer"
+event_fighters1 = ["Renato Moicano", "Virna Jandiroba", "Abdul Rakhman Yakhyaev", "Alessandro Costa", "Lando Vannata", "Azamat Bekoev", "Dione Barbosa"] 
 
-event_fighters1 = ["Israel Adesanya", "Alexa Grasso", "Michael Chiesa", "Mansur Abdul-Malik", "Terrance McKinney", "Casey O'Neill", "Navajo Stirling",
-                  "Ricky Simon", "Alexia Thainara"] 
+event_fighters2 =  ["Chris Duncan", "Tabatha Ricci", "Brendson Ribeiro", "Stewart Nicoll", "Darrius Flowers", "Tresean Gore", "Melissa Gatto"] 
 
-event_fighters2 =  ["Joe Pyfer", "Maycee Barber", "Niko Price", "Yousri Belgaroui", "Kyle Nelson", "Gabriella Fernandes", "Bruno Lopes",
-                  "Adrian Yanez", "Bruna Brasil"] 
+odds_fighters1 = [2.48, 2.02, 1.05, 1.22, 1.45, 1.16, 1.70]
+odds_fighters2 = [1.47, 1.71, 7.69, 3.74, 2.52, 4.61, 2.05]
 
-odds_fighters1 = [1.60, 2.06, 1.20, 1.77, 1.67, 2.00, 1.21, 1.57, 1.16]
-odds_fighters2 = [2.20, 1.69, 3.98, 1.95, 2.10, 1.73, 3.84, 2.25, 4.54]
-
-hit = [-1,-1,-1,-1, -1,-1,-1,-1, -1]
+hit = [-1,-1-1,-1, -1,-1,-1]
 
 event_accuracy = 0
 event_roi = 0
