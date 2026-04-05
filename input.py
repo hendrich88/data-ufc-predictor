5504232691ac9a19fb2b28e2119a8ea544f20cd0
hendrich88 <hendrich88@gmail.com>
2026-04-05 10:27:04 +0200

    Update input.py

--- input.py
+++ input.py
@@ -6,16 +6,16 @@ min_loser_fights = 0
 event_date = "2026-04-11"
 event = "UFC 327: Prochazka vs. Ulberg"
 
-event_fighters1 = ["Jiri Prochazka", "Joshua Van", "Curtis Blaydes", "Dominick Reyes", "Azamat Murzakanov", "Cub Swanson", "Mateusz Gamrot",
+event_fighters1 = ["Jiri Prochazka", "Curtis Blaydes", "Dominick Reyes", "Azamat Murzakanov", "Cub Swanson", "Mateusz Gamrot",
                   "Kevin Holland", "Tatiana Suarez", "Kelvin Gastelum"] 
 
-event_fighters2 =  ["Carlos Ulberg", "Tatsuro Taira", "Josh Hokit", "Johnny Walker", "Paulo Costa", "Nate Landwehr", "Esteban Ribovics",
+event_fighters2 =  ["Carlos Ulberg", "Josh Hokit", "Johnny Walker", "Paulo Costa", "Nate Landwehr", "Esteban Ribovics",
                   "Randy Brown", "Loopy Godinez", "Vicente Luque"] 
 
-odds_fighters1 = []
-odds_fighters2 = []
+odds_fighters1 = [1.45, 1.68, 1.75, 1.43, 2.11, 1.54, 2.09, 1.61, 1.37]
+odds_fighters2 = [2.54, 2.07, 1.98, 2.58, 1.66, 2.31, 1.67, 2.19, 2.77]
 
-hit = []
+hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
