c35cc69285cd57ce8adb7fe5f08547065aa8d8d2
hendrich88 <hendrich88@gmail.com>
2026-04-19 15:55:39 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,17 +3,19 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-04-18"
-event = "UFC Fight Night: Burns vs. Malott"
+event_date = "2026-04-25"
+event = "UFC Fight Night: Sterling vs. Zalal"
 
-event_fighters1 = ["Gilbert Burns", "Kyler Phillips", "Jasmine Jasudavicius", "Thiago Moises", "Melissa Croden", "Mitch Raposo", "JJ Aldrich"] 
+event_fighters1 = ["Aljamain Sterling", "Norma Dumont", "Rafa Garcia", "Montel Jackson", "Marcus Buchecha", "Rodolfo Vieira", 
+                   "Jackson McVey", "Jafel Filho", "Talita Alencar"] 
 
-event_fighters2 =  ["Mike Malott", "Charles Jourdain", "Karine Silva", "Gauge Young", "Daria Zhelezniakova", "Allan Nascimento", "Jamey-Lyn Horth"] 
+event_fighters2 =  ["Youssef Zalal", "Joselyne Edwards", "Alexander Hernandez", "Raoni Barcelos", "Ryan Spann", "Eric McConico", 
+                    "Sedriques Dumas", "Lucas Rocha", "Julia Polastri"] 
 
-odds_fighters1 = [4.38, 2.30, 1.30, 1.96, 1.57, 2.83, 2.53]
-odds_fighters2 = [1.17, 1.55, 3.10, 1.77, 2.26, 1.36, 1.45]
+odds_fighters1 = [2.25, 1.54, 2.12, 1.62, 1.67, 1.37, 1.48, 1.31, 2.69]
+odds_fighters2 = [1.58, 2.31, 1.66, 2.19, 2.09, 2.77, 2.45, 3.08, 1.40]
 
-hit = [-1, -1, -1, -1, -1, -1, -1]
+hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
