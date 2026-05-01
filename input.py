c97ed03bd86592d1682dea3bdb8bd1fa0df5daaa
hendrich88 <hendrich88@gmail.com>
2026-05-01 16:30:21 +0200

    Update input.py

--- input.py
+++ input.py
@@ -7,13 +7,13 @@ event_date = "2026-05-02"
 event = "UFC Fight Night: Della Maddalena vs. Prates"
 
 event_fighters1 = ["Jack Della Maddalena", "Beneil Dariush", "Tim Elliott", "Shamil Gaziev", "Cam Rowston", 
-                   "Junior Tafa", "Jacob Malkoun", "Colby Thicknesse", "Jonathan Micallef", "Dom Mar Fan"] 
+                   "Junior Tafa", "Jacob Malkoun", "Colby Thicknesse", "Jonathan Micallef", "Dom Mar Fan", "Tai Tuivasa"] 
 
 event_fighters2 =   ["Carlos Prates", "Quillan Salkilld", "Steve Erceg", "Brando Pericic", "Robert Bryczek", 
-                   "Kevin Christian", "Gerald Meerschaert", "Vince Morales", "Themba Gorimbo", "Kody Steele"] 
+                   "Kevin Christian", "Gerald Meerschaert", "Vince Morales", "Themba Gorimbo", "Kody Steele", "Louie Sutherland"] 
 
-odds_fighters1 = [1.92, 4.13, 3.59, 2.08, 1.42, 1.53, 1.08, 1.60, 1.27, 2.57]
-odds_fighters2 = [1.80, 1.19, 1.24, 1.68, 2.61, 2.33, 6.14, 2.20, 3.35, 1.43]
+odds_fighters1 = [1.92, 4.13, 3.59, 2.08, 1.42, 1.53, 1.08, 1.60, 1.27, 2.57, 1.46]
+odds_fighters2 = [1.80, 1.19, 1.24, 1.68, 2.61, 2.33, 6.14, 2.20, 3.35, 1.43, 2.71]
 
 hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 
