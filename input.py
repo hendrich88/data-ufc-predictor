ed110927212cf288a72f6dbf63d0d374536b967a
hendrich88 <hendrich88@gmail.com>
2026-05-02 16:47:28 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,19 +3,19 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-05-02"
-event = "UFC Fight Night: Della Maddalena vs. Prates"
+event_date = "2026-05-09"
+event = "UFC 328: Chimaev vs. Strickland"
 
-event_fighters1 = ["Jack Della Maddalena", "Beneil Dariush", "Tim Elliott", "Shamil Gaziev", "Cam Rowston", 
-                   "Junior Tafa", "Jacob Malkoun", "Colby Thicknesse", "Jonathan Micallef", "Dom Mar Fan", "Tai Tuivasa"] 
+event_fighters1 = ["Khamzat Chimaev", "Joshua Van", "Alexander Volkov", "Sean Brady", "King Green", "Ateba Gautier", "Grant Dawson",
+                   "Jim Miller", "Roman Kopylov", "Pat Sabatini", "Baisangur Susurkaev", "Clayton Carpenter"] 
 
-event_fighters2 =   ["Carlos Prates", "Quillan Salkilld", "Steve Erceg", "Brando Pericic", "Robert Bryczek", 
-                   "Kevin Christian", "Gerald Meerschaert", "Vince Morales", "Themba Gorimbo", "Kody Steele", "Louie Sutherland"] 
+event_fighters2 =   ["Sean Strickland", "Tatsuro Taira", "Waldo Cortes Acosta", "Joaquin Buckley", "Jeremy Stephens", "Ozzy Diaz", "Mateusz Rebecki",
+                   "Jared Gordon", "Marco Tulio", "William Gomis", "Djorden Santos", "Jose Ochoa"] 
 
-odds_fighters1 = [1.92, 4.13, 3.59, 2.08, 1.42, 1.53, 1.08, 1.60, 1.27, 2.57, 1.46]
-odds_fighters2 = [1.80, 1.19, 1.24, 1.68, 2.61, 2.33, 6.14, 2.20, 3.35, 1.43, 2.71]
+odds_fighters1 = [1.17, 2.44, 1.57, 1.48, 1.33, 1.09, 1.65, 3.13, 2.37, 1.45, 1.10, 2.29]
+odds_fighters2 = [4.49, 1.48, 2.26, 2.45, 2.97, 5.98, 2.13, 1.30, 1.51, 2.51, 5.62, 1.55]
 
-hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
