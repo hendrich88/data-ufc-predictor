c15fe17833a63f18861f9df49f4e49b2e34976ea
hendrich88 <hendrich88@gmail.com>
2026-05-12 21:37:04 +0200

    Update input.py

--- input.py
+++ input.py
@@ -6,17 +6,18 @@ min_loser_fights = 0
 event_date = "2026-05-16"
 event = "UFC Fight Night: Allen vs. Costa"
 
-event_fighters1 = ["Arnold Allen", "Dooho Choi", "Modestas Bukauskas", "Timmy Cuamba", "Jeremiah Wells", "Tuco Tokkos", "Ketlen Vieira",
-                   "Cody Brundage", "Alice Ardelean", "Daniel Barez", "Shauna Bannon"] 
+event_fighters1 = ["Arnold Allen", "Dooho Choi", "Modestas Bukauskas", "Timmy Cuamba", "Tuco Tokkos", "Ketlen Vieira",
+                   "Cody Brundage", "Alice Ardelean", "Daniel Barez", "Shauna Bannon", "Nikolay Veretennikov"] 
 
-event_fighters2 =   ["Melquizael Costa", "Daniel Santos", "Rodolfo Bellato", "Benardo Sopaj", "Nicolas Dalby", "Ivan Erslan", "Jacqueline Cavalcanti",
-                   "Andre Petroski", "Polyana Viana", "Luis Gurule", "Nicolle Caliari"]
+event_fighters2 =   ["Melquizael Costa", "Daniel Santos", "Rodolfo Bellato", "Benardo Sopaj", "Ivan Erslan", "Jacqueline Cavalcanti",
+                   "Andre Petroski", "Polyana Viana", "Luis Gurule", "Nicolle Caliari", "Khaos Williams"]
 
-odds_fighters1 = [1.51, 1.98, 1.72, 2.21, None, 2.26, 2.19, 2.57, 1.50, 1.86, 2.42]
+odds_fighters1 = [1.56, 2.30, 1.84, 2.21, 2.34, 2.30, 2.55, 1.46, 1.89, 2.55, 1.93]
 
-odds_fighters2 = [2.38, 1.75, 2.02, 1.60, None, 1.57, 1.61, 1.43, 2.40, 1.86, 1.49]
+odds_fighters2 = [2.27, 1.55, 1.88, 1.60, 1.53, 1.55, 1.44, 2.50, 1.83, 1.44, 1.79]
 
-hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+hit = [-1, -1, -1, -1, -1, -1, -1, 
+       -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
