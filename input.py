e1b4244403e13dbeb1675d449bce622e36acd4fb
hendrich88 <hendrich88@gmail.com>
2026-05-17 08:25:48 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,21 +3,43 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-05-16"
-event = "UFC Fight Night: Allen vs. Costa"
+event_date = "2026-05-30"
+event = "UFC Fight Night: Song vs. Figueiredo"
 
-event_fighters1 = ["Arnold Allen", "Dooho Choi", "Modestas Bukauskas", "Timmy Cuamba", "Tuco Tokkos", "Ketlen Vieira",
-                   "Cody Brundage", "Alice Ardelean", "Daniel Barez", "Shauna Bannon", "Nikolay Veretennikov"] 
+event_fighters1 = ["Song Yadong", "Zhang Mingyang", "Sergei Pavlovich", "Kai Asakura", "Muslim Salikhov", "Alex Perez", "Aoriqileng",
+                   "Rei Tsuruya", "Zhu Kangjie", "Loma Lookboonmee"] 
 
-event_fighters2 =   ["Melquizael Costa", "Daniel Santos", "Rodolfo Bellato", "Benardo Sopaj", "Ivan Erslan", "Jacqueline Cavalcanti",
-                   "Andre Petroski", "Polyana Viana", "Luis Gurule", "Nicolle Caliari", "Khaos Williams"]
+event_fighters2 =   ["Deiveson Figueiredo", "Alonzo Menifield", "Tallison Teixeira", "Cameron Smotherman", "Jake Matthews", "Sumudaerji", "Cody Haddon",
+                   "Jesus Aguilar", "Ramon Taveras", "Jaqueline Amorim"] 
 
-odds_fighters1 = [1.56, 2.30, 1.84, 2.21, 2.34, 2.30, 2.55, 1.46, 1.89, 2.55, 1.93]
+odds_fighters1 = [
+    1.22,  # Song Yadong
+    1.42,  # Zhang Mingyang
+    1.23,  # Sergei Pavlovich
+    1.29,  # Kai Asakura
+    None,  # Muslim Salikhov
+    1.74,  # Alex Perez
+    3.35,  # Aoriqileng
+    1.30,  # Rei Tsuruya
+    2.30,  # Zhu Kangjie
+    2.14   # Loma Lookboonmee
+]
 
-odds_fighters2 = [2.27, 1.55, 1.88, 1.60, 1.53, 1.55, 1.44, 2.50, 1.83, 1.44, 1.79]
+odds_fighters2 = [
+    3.75,  # Deiveson Figueiredo
+    2.60,  # Alonzo Menifield
+    3.68,  # Tallison Teixeira
+    3.17,  # Cameron Smotherman
+    None,  # Jake Matthews
+    1.98,  # Sumudaerji
+    1.27,  # Cody Haddon
+    3.13,  # Jesus Aguilar
+    1.55,  # Ramon Taveras
+    1.64   # Jaqueline Amorim
+]
 
 hit = [-1, -1, -1, -1, -1, -1, -1, 
-       -1, -1, -1, -1]
+       -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
