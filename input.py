ea66e6acf16aef325be7c33c0392a587e02af0d1
hendrich88 <hendrich88@gmail.com>
2026-05-31 20:01:09 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,21 +3,44 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-05-30"
-event = "UFC Fight Night: Song vs. Figueiredo"
+event_date = "2026-06-06"
+event = "UFC Fight Night: Muhammad vs. Bonfim"
 
-event_fighters1 = ["Song Yadong", "Zhang Mingyang", "Sergei Pavlovich", "Kai Asakura", "Carlston Harris", "Alex Perez", "Aoriqileng",
-                   "Rei Tsuruya", "Loma Lookboonmee"] 
+event_fighters1 = ["Belal Muhammad", "Brendan Allen", "Fares Ziam", "Bryce Mitchell", "Iwo Baraniewski", "Matt Schnell", "Marcus McGhee",
+                   "Bruno Silva", "Priscila Cachoeira", "Jordan Leavitt", "Ketlen Souza"] 
 
-event_fighters2 =   ["Deiveson Figueiredo", "Alonzo Menifield", "Tallison Teixeira", "Cameron Smotherman", "Jake Matthews", "Sumudaerji", "Cody Haddon",
-                   "Luis Gurule", "Jaqueline Amorim"] 
+event_fighters2 =   ["Gabriel Bonfim", "Edmen Shahbazyan", "Tom Nolan", "Santiago Luna", "Junior Tafa", "Alessandro Costa", "John Yannis",
+                   "Edgar Chairez", "Chelsea Chandler", "Joanderson Brito", "Ariane Carnelossi"]  
 
-odds_fighters1 = [1.15, 1.36, 1.15, 1.32, 3.28, 1.68, 3.48, 1.40, 1.97]
+odds_fighters1 = [
+    1.76,  # Muhammad
+    1.39,  # Allen
+    1.31,  # Ziam
+    None,  # Mitchell (kurz není v dodaném seznamu)
+    1.31,  # Baraniewski
+    None,  # Schnell (kurz není v dodaném seznamu)
+    1.18,  # McGhee
+    1.74,  # Silva
+    2.90,  # Cachoeira
+    2.35,  # Leavitt
+    1.34   # Souza
+]
 
-odds_fighters2 = [4.83, 2.85, 4.81, 3.09, 1.29, 2.11, 1.25, 2.66, 1.76]
+odds_fighters2 = [
+    1.96,  # Bonfim
+    2.71,  # Shahbazyan
+    3.10,  # Nolan
+    None,  # Luna (kurz není v dodaném seznamu)
+    3.05,  # Tafa
+    None,  # Costa (kurz není v dodaném seznamu)
+    4.24,  # Yannis
+    2.00,  # Chairez
+    1.79,  # Chandler
+    1.52,  # Brito
+    2.90   # Carnelossi
 
 hit = [-1, -1, -1, -1, -1, -1, -1, 
-       -1, -1]
+       -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
