fe6b096c6aaddf0ad442ce5bc295c10b2329552c
hendrich88 <hendrich88@gmail.com>
2026-06-02 22:03:58 +0200

    Update input.py

--- input.py
+++ input.py
@@ -7,37 +7,41 @@ event_date = "2026-06-06"
 event = "UFC Fight Night: Muhammad vs. Bonfim"
 
 event_fighters1 = ["Belal Muhammad", "Brendan Allen", "Fares Ziam",  "Iwo Baraniewski", "Marcus McGhee",
-                   "Bruno Silva", "Priscila Cachoeira", "Jordan Leavitt", "Ketlen Souza"] 
+                   "Bruno Silva", "Priscila Cachoeira", "Jordan Leavitt", "Ketlen Souza", "Bryce Mitchell", "Matt Schnell"] 
 
 event_fighters2 =   ["Gabriel Bonfim", "Edmen Shahbazyan", "Tom Nolan",  "Junior Tafa", "John Yannis",
-                   "Edgar Chairez", "Chelsea Chandler", "Joanderson Brito", "Ariane Carnelossi"]  
+                   "Edgar Chairez", "Chelsea Chandler", "Joanderson Brito", "Ariane Carnelossi", "Santiago Luna", "Alessandro Costa"]  
 
 odds_fighters1 = [
-    1.76,  # Muhammad
-    1.39,  # Allen
-    1.31,  # Ziam
-    1.31,  # Baraniewski
-    1.18,  # McGhee
-    1.74,  # Silva
-    2.90,  # Cachoeira
-    2.35,  # Leavitt
-    1.34   # Souza
+    1.76,  # Belal Muhammad
+    1.42,  # Brendan Allen
+    1.28,  # Fares Ziam
+    1.24,  # Iwo Baraniewski
+    1.17,  # Marcus McGhee
+    1.67,  # Bruno Silva
+    1.98,  # Priscila Cachoeira
+    2.43,  # Jordan Leavitt
+    1.30,  # Ketlen Souza
+    1.64,  # Bryce Mitchell
+    5.32   # Matt Schnell
 ]
 
 odds_fighters2 = [
-    1.96,  # Bonfim
-    2.71,  # Shahbazyan
-    3.10,  # Nolan
-    3.05,  # Tafa
-    4.24,  # Yannis
-    2.00,  # Chairez
-    1.79,  # Chandler
-    1.52,  # Brito
-    2.90   # Carnelossi
+    1.97,  # Gabriel Bonfim
+    2.61,  # Edmen Shahbazyan
+    3.25,  # Tom Nolan
+    3.56,  # Junior Tafa
+    4.34,  # John Yannis
+    2.09,  # Edgar Chairez
+    1.75,  # Chelsea Chandler
+    1.49,  # Joanderson Brito
+    3.13,  # Ariane Carnelossi
+    2.13,  # Santiago Luna
+    1.11   # Alessandro Costa
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1, 
-       -1, -1]
+       -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
