e749bbc5b7f566c414cb2418ff1af0871806b85c
hendrich88 <hendrich88@gmail.com>
2026-06-08 10:51:09 +0200

    Update input.py

--- input.py
+++ input.py
@@ -10,9 +10,25 @@ event_fighters1 = ["Ilia Topuria", "Alex Pereira", "Sean O'Malley",  "Josh Hokit
 
 event_fighters2 =   ["Justin Gaethje", "Ciryl Gane", "Aiemann Zahabi",  "Derrick Lewis", "Michael Chandler", "Kyle Daukaus", "Steve Garcia"] 
 
-odds_fighters1 = []
+odds_fighters1 = [
+    1.16,  # Topuria
+    1.89,  # Pereira
+    1.21,  # O'Malley
+    1.22,  # Hokit
+    1.13,  # Ruffy
+    1.26,  # Nickal
+    1.58   # Lopes
+]
 
-odds_fighters2 = []
+odds_fighters2 = [
+    4.61,  # Gaethje
+    1.84,  # Gane
+    3.86,  # Zahabi
+    3.72,  # Lewis
+    5.07,  # Chandler
+    3.38,  # Daukaus
+    2.24   # Garcia
+]
 
 hit = [-1, -1, -1, -1, -1, -1, -1]
 
