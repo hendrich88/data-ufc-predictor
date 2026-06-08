fda7651bf311c10e54c0ffa76ad28c460f474f82
hendrich88 <hendrich88@gmail.com>
2026-06-08 10:47:24 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,45 +3,18 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-06-06"
-event = "UFC Fight Night: Muhammad vs. Bonfim"
+event_date = "2026-06-14"
+event = "UFC Freedom 250"
 
-event_fighters1 = ["Belal Muhammad", "Brendan Allen", "Fares Ziam",  "Iwo Baraniewski", "Marcus McGhee",
-                   "Bruno Silva", "Priscila Cachoeira", "Jordan Leavitt", "Ketlen Souza", "Bryce Mitchell", "Matt Schnell"] 
+event_fighters1 = ["Ilia Topuria", "Alex Pereira", "Sean O'Malley",  "Josh Hokit", "Mauricio Ruffy", "Bo Nickal", "Diego Lopes"] 
 
-event_fighters2 =   ["Gabriel Bonfim", "Edmen Shahbazyan", "Tom Nolan",  "Junior Tafa", "John Yannis",
-                   "Edgar Chairez", "Chelsea Chandler", "Joanderson Brito", "Ariane Carnelossi", "Santiago Luna", "Alessandro Costa"]  
+event_fighters2 =   ["Justin Gaethje", "Ciryl Gane", "Aiemann Zahabi",  "Derrick Lewis", "Michael Chandler", "Kyle Daukaus", "Steve Garcia"] 
 
-odds_fighters1 = [
-    1.76,  # Belal Muhammad
-    1.42,  # Brendan Allen
-    1.28,  # Fares Ziam
-    1.24,  # Iwo Baraniewski
-    1.17,  # Marcus McGhee
-    1.67,  # Bruno Silva
-    1.98,  # Priscila Cachoeira
-    2.43,  # Jordan Leavitt
-    1.30,  # Ketlen Souza
-    1.64,  # Bryce Mitchell
-    5.32   # Matt Schnell
-]
+odds_fighters1 = []
 
-odds_fighters2 = [
-    1.97,  # Gabriel Bonfim
-    2.61,  # Edmen Shahbazyan
-    3.25,  # Tom Nolan
-    3.56,  # Junior Tafa
-    4.34,  # John Yannis
-    2.09,  # Edgar Chairez
-    1.75,  # Chelsea Chandler
-    1.49,  # Joanderson Brito
-    3.13,  # Ariane Carnelossi
-    2.13,  # Santiago Luna
-    1.11   # Alessandro Costa
-]
+odds_fighters2 = []
 
-hit = [-1, -1, -1, -1, -1, -1, -1, 
-       -1, -1, -1, -1]
+hit = [-1, -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
