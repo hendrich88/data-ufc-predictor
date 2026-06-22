5973338d7cabed175158171161c332bd22f2efdb
hendrich88 <hendrich88@gmail.com>
2026-06-22 18:39:58 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,41 +3,43 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-06-20"
-event = "UFC Fight Night: Kape vs. Horiguchi"
+event_date = "2026-06-27"
+event = "UFC Fight Night: Fiziev vs. Torres"
 
-event_fighters1 = ["Manel Kape", "Ion Cutelaba", "Vinicius Oliveira",  "Hyder Amil", "Andre Lima", "Bia Mesquita", "Allan Nascimento",
-                  "Gaston Bolanos", "Karol Rosa"] 
+event_fighters1 = ["Rafael Fiziev", "Shara Magomedov", "Nazim Sadykhov",  "Asu Almabayev", "Ikram Aliskerov", "Abus Magomedov", "Abdul Rakhman Yakhyaev",
+                  "Nursulton Ruziboev", "Kaan Ofli", "Bekzat Almakhan"] 
 
-event_fighters2 =   ["Kyoji Horiguchi", "Navajo Stirling", "Andre Fili",  "Christian Rodriguez", "Kevin Borjas", "Melissa Mullins", "Mitch Raposo",
-                  "Michael Aswell Jr.", "Luana Santos"] 
+event_fighters2 =   ["Manuel Torres", "Michel Pereira", "Matheus Camilo",  "Charles Johnson", "Brunno Ferreira", "Michal Oleksiejczuk", "Julius Walker",
+                  "Andrey Pulyaev", "Javier Reyes", "Jean Matsumoto"] 
 
 odds_fighters1 = [
-    1.58,  # Manel Kape
-    3.13,  # Ion Cutelaba
-    1.38,  # Vinicius Oliveira
-    2.22,  # Hyder Amil
-    1.15,  # Andre Lima
-    1.15,  # Bia Mesquita
-    1.48,  # Allan Nascimento
-    3.59,  # Gaston Bolanos
-    1.72   # Karol Rosa
+    1.82,  # Rafael Fiziev
+    1.26,  # Shara Magomedov
+    1.38,  # Nazim Sadykhov
+    1.40,  # Asu Almabayev
+    1.32,  # Ikram Aliskerov
+    1.69,  # Abus Magomedov
+    1.17,  # Abdul Rakhman Yakhyaev
+    1.42,  # Nursulton Ruziboev
+    2.26,  # Kaan Ofli
+    2.22   # Bekzat Almakhan
 ]
 
 odds_fighters2 = [
-    2.24,  # Kyoji Horiguchi
-    1.30,  # Navajo Stirling
-    2.74,  # Andre Fili
-    1.59,  # Christian Rodriguez
-    4.71,  # Kevin Borjas
-    4.75,  # Melissa Mullins
-    2.46,  # Mitch Raposo
-    1.24,  # Michael Aswell Jr.
-    2.02   # Luana Santos
+    1.90,  # Manuel Torres
+    3.37,  # Michel Pereira
+    2.76,  # Matheus Camilo
+    2.67,  # Charles Johnson
+    3.01,  # Brunno Ferreira
+    2.06,  # Michal Oleksiejczuk
+    4.49,  # Julius Walker
+    2.61,  # Andrey Pulyaev
+    1.57,  # Javier Reyes
+    1.59   # Jean Matsumoto
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1,
-      -1, -1]
+      -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
