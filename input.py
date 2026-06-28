3f50b18d0f72f947c8d1cae98897463b60973e93
hendrich88 <hendrich88@gmail.com>
2026-06-28 20:49:47 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,43 +3,49 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-06-27"
-event = "UFC Fight Night: Fiziev vs. Torres"
+event_date = "2026-07-11"
+event = "UFC 329: McGregor vs. Holloway 2"
 
-event_fighters1 = ["Rafael Fiziev", "Shara Magomedov", "Nazim Sadykhov",  "Asu Almabayev", "Ikram Aliskerov", "Abus Magomedov", "Abdul Rakhman Yakhyaev",
-                  "Nursulton Ruziboev", "Kaan Ofli", "Bekzat Almakhan"] 
+event_fighters1 = ["Conor McGregor", "Benoit Saint Denis", "Cory Sandhagen",  "Brandon Royval", "King Green", "Nikita Krylov", "Cody Garbrandt",
+                  "Luke Riley", "Tracy Cortez", "Damian Pinas",  "Farid Basharat", "Ryan Gandra", "Ode Osbourne"] 
 
-event_fighters2 =   ["Manuel Torres", "Michel Pereira", "Matheus Camilo",  "Charles Johnson", "Brunno Ferreira", "Michal Oleksiejczuk", "Julius Walker",
-                  "Andrey Pulyaev", "Javier Reyes", "Jean Matsumoto"] 
+event_fighters2 =   ["Max Holloway", "Paddy Pimblett", "Mario Bautista",  "Lone'er Kavanagh", "Terrance McKinney", "Robert Whittaker", "Adrian Yanez",
+                  "Kai Kamaka III", "Wang Cong", "Cesar Almeida",  "Ethyn Ewing", "Zach Reese", "Cody Durden"] 
 
 odds_fighters1 = [
-    1.82,  # Rafael Fiziev
-    1.26,  # Shara Magomedov
-    1.38,  # Nazim Sadykhov
-    1.40,  # Asu Almabayev
-    1.32,  # Ikram Aliskerov
-    1.69,  # Abus Magomedov
-    1.17,  # Abdul Rakhman Yakhyaev
-    1.42,  # Nursulton Ruziboev
-    2.26,  # Kaan Ofli
-    2.22   # Bekzat Almakhan
+    2.74,  # Conor McGregor
+    1.61,  # Benoit Saint Denis
+    1.62,  # Cory Sandhagen
+    2.51,  # Brandon Royval
+    2.30,  # King Green (Bobby Green)
+    2.30,  # Nikita Krylov
+    3.48,  # Cody Garbrandt
+    1.30,  # Luke Riley
+    2.12,  # Tracy Cortez
+    1.55,  # Damian Pinas
+    # Farid Basharat - missing odds
+    1.70,  # Ryan Gandra
+    # Ode Osbourne - missing odds
 ]
 
 odds_fighters2 = [
-    1.90,  # Manuel Torres
-    3.37,  # Michel Pereira
-    2.76,  # Matheus Camilo
-    2.67,  # Charles Johnson
-    3.01,  # Brunno Ferreira
-    2.06,  # Michal Oleksiejczuk
-    4.49,  # Julius Walker
-    2.61,  # Andrey Pulyaev
-    1.57,  # Javier Reyes
-    1.59   # Jean Matsumoto
+    1.39,  # Max Holloway
+    2.19,  # Paddy Pimblett
+    2.18,  # Mario Bautista
+    1.46,  # Lone'er Kavanagh
+    1.55,  # Terrance McKinney
+    1.55,  # Robert Whittaker
+    1.25,  # Adrian Yanez
+    3.13,  # Kai Kamaka III
+    1.65,  # Wang Cong
+    2.30,  # Cesar Almeida
+    # Ethyn Ewing - missing odds
+    2.05,  # Zach Reese
+    # Cody Durden - missing odds
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1]
+      -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
