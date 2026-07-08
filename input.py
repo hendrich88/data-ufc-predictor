b6731351aa4aa0fd70a0c0b20724f1ebf8ff2c8b
hendrich88 <hendrich88@gmail.com>
2026-07-08 12:07:59 +0200

    Update input.py

--- input.py
+++ input.py
@@ -7,45 +7,43 @@ event_date = "2026-07-11"
 event = "UFC 329: McGregor vs. Holloway 2"
 
 event_fighters1 = ["Conor McGregor", "Benoit Saint Denis", "Cory Sandhagen",  "Brandon Royval", "King Green", "Nikita Krylov", "Cody Garbrandt",
-                  "Luke Riley", "Tracy Cortez", "Damian Pinas",  "Farid Basharat", "Ryan Gandra", "Ode Osbourne"] 
+                  "Luke Riley", "Tracy Cortez", "Damian Pinas",  "Ryan Gandra", "Alessandro Costa"] 
 
 event_fighters2 =   ["Max Holloway", "Paddy Pimblett", "Mario Bautista",  "Lone'er Kavanagh", "Terrance McKinney", "Robert Whittaker", "Adrian Yanez",
-                  "Kai Kamaka III", "Wang Cong", "Cesar Almeida",  "Ethyn Ewing", "Zach Reese", "Cody Durden"] 
+                  "Kai Kamaka III", "Wang Cong", "Cesar Almeida", "Zach Reese", "Cody Durden"] 
 
 odds_fighters1 = [
-    2.74,  # Conor McGregor
-    1.61,  # Benoit Saint Denis
-    1.62,  # Cory Sandhagen
-    2.51,  # Brandon Royval
-    2.30,  # King Green (Bobby Green)
-    2.30,  # Nikita Krylov
-    3.48,  # Cody Garbrandt
-    1.30,  # Luke Riley
-    2.12,  # Tracy Cortez
-    1.55,  # Damian Pinas
-    # Farid Basharat - missing odds
+    2.61,  # Conor McGregor
+    1.67,  # Benoit Saint Denis
+    1.65,  # Cory Sandhagen
+    2.59,  # Brandon Royval
+    2.13,  # King Green
+    2.05,  # Nikita Krylov
+    3.82,  # Cody Garbrandt
+    1.33,  # Luke Riley
+    1.88,  # Tracy Cortez
+    1.43,  # Damian Pinas
     1.70,  # Ryan Gandra
-    # Ode Osbourne - missing odds
+    1.37,  # Alessandro Costa
 ]
 
 odds_fighters2 = [
-    1.39,  # Max Holloway
-    2.19,  # Paddy Pimblett
-    2.18,  # Mario Bautista
-    1.46,  # Lone'er Kavanagh
-    1.55,  # Terrance McKinney
-    1.55,  # Robert Whittaker
-    1.25,  # Adrian Yanez
-    3.13,  # Kai Kamaka III
-    1.65,  # Wang Cong
-    2.30,  # Cesar Almeida
-    # Ethyn Ewing - missing odds
+    1.43,  # Max Holloway
+    2.09,  # Paddy Pimblett
+    2.12,  # Mario Bautista
+    1.43,  # Lone'er Kavanagh
+    1.65,  # Terrance McKinney
+    1.69,  # Robert Whittaker
+    1.21,  # Adrian Yanez
+    2.96,  # Kai Kamaka III
+    1.84,  # Wang Cong
+    2.57,  # Cesar Almeida
     2.05,  # Zach Reese
-    # Cody Durden - missing odds
+    2.79,  # Cody Durden
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1]
+      -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
