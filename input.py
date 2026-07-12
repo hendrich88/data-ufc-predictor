c28ac0fc5ee8331e45780a30b8d778875bfcba04
hendrich88 <hendrich88@gmail.com>
2026-07-12 19:48:06 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,47 +3,39 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-07-11"
-event = "UFC 329: McGregor vs. Holloway 2"
+event_date = "2026-07-18"
+event = "UFC Fight Night: Du Plessis vs. Usman"
 
-event_fighters1 = ["Conor McGregor", "Benoit Saint Denis", "Cory Sandhagen",  "Brandon Royval", "King Green", "Nikita Krylov", "Cody Garbrandt",
-                  "Luke Riley", "Tracy Cortez", "Damian Pinas",  "Ryan Gandra", "Alessandro Costa"] 
+event_fighters1 = ["Dricus Du Plessis", "Jared Cannonier", "Chase Hooper",  "Tabatha Ricci", "Tommy McMillen", "Austin Bashi", "Jean-Paul Lebosnoyani",
+                  "Alden Coria"] 
 
-event_fighters2 =   ["Max Holloway", "Paddy Pimblett", "Mario Bautista",  "Lone'er Kavanagh", "Terrance McKinney", "Robert Whittaker", "Adrian Yanez",
-                  "Kai Kamaka III", "Wang Cong", "Cesar Almeida", "Zach Reese", "Cody Durden"] 
+event_fighters2 =   ["Kamaru Usman", "Christian Leroy Duncan", "Mitch Ramirez",  "Fatima Kline", "Alberto Montes", "Jose Delgado", "Seokhyeon Ko",
+                  "Stewart Nicoll"] 
 
 odds_fighters1 = [
-    2.61,  # Conor McGregor
-    1.67,  # Benoit Saint Denis
-    1.65,  # Cory Sandhagen
-    2.59,  # Brandon Royval
-    2.13,  # King Green
-    2.05,  # Nikita Krylov
-    3.82,  # Cody Garbrandt
-    1.33,  # Luke Riley
-    1.88,  # Tracy Cortez
-    1.43,  # Damian Pinas
-    1.70,  # Ryan Gandra
-    1.37,  # Alessandro Costa
+    1.33,  # Dricus Du Plessis
+    3.19,  # Jared Cannonier
+    1.26,  # Chase Hooper
+    3.32,  # Tabatha Ricci
+    1.88,  # Tommy McMillen
+    1.94,  # Austin Bashi
+    2.50,  # Jean-Paul Lebosnoyani
+    1.09,  # Alden Coria
 ]
 
 odds_fighters2 = [
-    1.43,  # Max Holloway
-    2.09,  # Paddy Pimblett
-    2.12,  # Mario Bautista
-    1.43,  # Lone'er Kavanagh
-    1.65,  # Terrance McKinney
-    1.69,  # Robert Whittaker
-    1.21,  # Adrian Yanez
-    2.96,  # Kai Kamaka III
-    1.84,  # Wang Cong
-    2.57,  # Cesar Almeida
-    2.05,  # Zach Reese
-    2.79,  # Cody Durden
+    2.96,  # Kamaru Usman
+    1.29,  # Christian Leroy Duncan
+    3.42,  # Mitch Ramirez
+    1.27,  # Fatima Kline
+    1.83,  # Alberto Montes
+    1.79,  # Jose Delgado
+    1.46,  # Seokhyeon Ko
+    5.98,  # Stewart Nicoll
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1]
+      -1]
 
 event_accuracy = 0
 event_roi = 0
