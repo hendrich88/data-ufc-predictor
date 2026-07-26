1086623011ffeee02b53618524c3172da8fd0de2
hendrich88 <hendrich88@gmail.com>
2026-07-26 21:07:38 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,34 +3,34 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-07-25"
-event = "UFC Fight Night: Ankalaev vs. Guskov"
+event_date = "2026-08-01"
+event = "UFC Fight Night: Medic vs. Rodriguez"
 
-event_fighters1 = ["Magomed Ankalaev", "Steve Erceg", "Islam Dulatov",  "Valter Walker", "Santiago Ponzinibbio", "Ismael Bonfim", "Nurullo Aliev"] 
+event_fighters1 = ["Uros Medic", "Jan Blachowicz", "Aleksandar Rakic",  "Dusko Todorovic", "Ludovit Klein",  "Dennis Buzukja", "Mateusz Rebecki"] 
 
-event_fighters2 =   ["Bogdan Guskov", "Ramazan Temirov", "Wellington Turman",  "Thomas Petersen", "Sam Patterson", "Axel Sola", "Mike Davis"] 
+event_fighters2 =   ["Daniel Rodriguez", "Navajo Stirling", "Marcin Tybura",  "Robert Valentin", "Tofiq Musayev", "Bogdan Grad", "Kyle Prepolec"] 
 
 odds_fighters1 = [
-    1.16,  # Magomed Ankalaev
-    1.78,  # Steve Erceg
-    1.11,  # Islam Dulatov
-    1.45,  # Valter Walker
-    3.75,  # Santiago Ponzinibbio
-    2.37,  # Ismael Bonfim
-    1.61,  # Nurullo Aliev
+    1.24,  # Uros Medic
+    3.22,  # Jan Blachowicz
+    1.26,  # Aleksandar Rakic
+    2.18,  # Dusko Todorovic
+    1.40,  # Ludovit Klein
+    2.36,  # Dennis Buzukja
+    1.19,  # Mateusz Rebecki
 ]
 
 odds_fighters2 = [
-    4.59,  # Bogdan Guskov
-    1.94,  # Ramazan Temirov
-    5.33,  # Wellington Turman
-    2.52,  # Thomas Petersen
-    1.22,  # Sam Patterson
-    1.51,  # Axel Sola
-    2.19,  # Mike Davis
+    3.56,  # Daniel Rodriguez
+    1.29,  # Navajo Stirling
+    3.38,  # Marcin Tybura
+    1.62,  # Robert Valentin
+    2.68,  # Tofiq Musayev
+    1.52,  # Bogdan Grad
+    4.08,  # Kyle Prepolec
 ]
 
-hit = [-1, -1, -1, -1, -1, -1, -1,]
+hit = [-1, -1, -1, -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
