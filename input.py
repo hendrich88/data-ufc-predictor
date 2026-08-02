b2404146a6c93dfadc1d12a620a35d22f88c8990
hendrich88 <hendrich88@gmail.com>
2026-08-02 22:18:02 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,34 +3,39 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-08-01"
-event = "UFC Fight Night: Medic vs. Rodriguez"
+event_date = "2026-08-08"
+event = "UFC Fight Night: Gamrot vs. Salkilld"
 
-event_fighters1 = ["Uros Medic", "Jan Blachowicz", "Aleksandar Rakic",  "Dusko Todorovic", "Ludovit Klein",  "Dennis Buzukja", "Mateusz Rebecki"] 
+event_fighters1 = ["Mateusz Gamrot", "Diego Ferreira", "Darren Elkins",  "Amanda Lemos", "Ty Miller",  "Diyar Nurgozhay", "Steven Asplund",
+                  "Juliana Miller"] 
 
-event_fighters2 =   ["Daniel Rodriguez", "Navajo Stirling", "Marcin Tybura",  "Robert Valentin", "Tofiq Musayev", "Bogdan Grad", "Kyle Prepolec"] 
+event_fighters2 =   ["Quillan Salkilld", "Billy Quarantillo", "Yadier del Valle",  "Alexia Thainara", "Billy Ray Goff",  "Bruno Lopes", "Guilherme Pat",
+                  "Ravena Oliveira"] 
 
 odds_fighters1 = [
-    1.24,  # Uros Medic
-    3.22,  # Jan Blachowicz
-    1.26,  # Aleksandar Rakic
-    2.18,  # Dusko Todorovic
-    1.40,  # Ludovit Klein
-    2.36,  # Dennis Buzukja
-    1.19,  # Mateusz Rebecki
+    2.11,  # Mateusz Gamrot
+    1.57,  # Diego Ferreira
+    5.07,  # Darren Elkins
+    2.79,  # Amanda Lemos
+    1.29,  # Ty Miller
+    1.62,  # Diyar Nurgozhay
+    1.37,  # Steven Asplund
+    1.37,  # Juliana Miller
 ]
 
 odds_fighters2 = [
-    3.56,  # Daniel Rodriguez
-    1.29,  # Navajo Stirling
-    3.38,  # Marcin Tybura
-    1.62,  # Robert Valentin
-    2.68,  # Tofiq Musayev
-    1.52,  # Bogdan Grad
-    4.08,  # Kyle Prepolec
+    1.66,  # Quillan Salkilld
+    2.27,  # Billy Quarantillo
+    1.13,  # Yadier del Valle
+    1.37,  # Alexia Thainara
+    3.22,  # Billy Ray Goff
+    2.18,  # Bruno Lopes
+    2.81,  # Guilherme Pat
+    2.78,  # Ravena Oliveira
 ]
 
-hit = [-1, -1, -1, -1, -1, -1, -1]
+hit = [-1, -1, -1, -1, -1, -1, -1,
+      -1]
 
 event_accuracy = 0
 event_roi = 0
