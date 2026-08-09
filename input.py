369147534404cde17235dbcf3e454d87c94909d4
hendrich88 <hendrich88@gmail.com>
2026-08-09 18:53:36 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,39 +3,45 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-08-08"
-event = "UFC Fight Night: Gamrot vs. Salkilld"
+event_date = "2026-08-15"
+event = "UFC 330: Makhachev vs. Machado Garry"
 
-event_fighters1 = ["Mateusz Gamrot", "Diego Ferreira", "Darren Elkins",  "Amanda Lemos", "Ty Miller",  "Diyar Nurgozhay", "Steven Asplund",
-                  "Juliana Miller"] 
+event_fighters1 = ["Islam Makhachev", "Mackenzie Dern", "Charles Johnson",  "Mansur Abdul-Malik", "Edson Barboza",  "Chidi Njokuani", "Jalin Turner",
+                   "Donte Johnson", "Vicente Luque", "Neil Magny", "Jeremiah Wells"] 
 
-event_fighters2 =   ["Quillan Salkilld", "Billy Quarantillo", "Yadier del Valle",  "Alexia Thainara", "Billy Ray Goff",  "Bruno Lopes", "Guilherme Pat",
-                  "Ravena Oliveira"] 
+event_fighters2 =   ["Ian Machado Garry", "Gillian Robertson", "Jose Ochoa",  "Dustin Stoltzfus", "Esteban Ribovics",  "Joel Alvarez", "Kaue Fernandes",
+                     "Eric McConico", "Tresean Gore", "Ramiz Brahimaj",  "Myktybek Orolbai"] 
 
 odds_fighters1 = [
-    2.11,  # Mateusz Gamrot
-    1.57,  # Diego Ferreira
-    5.07,  # Darren Elkins
-    2.79,  # Amanda Lemos
-    1.29,  # Ty Miller
-    1.62,  # Diyar Nurgozhay
-    1.37,  # Steven Asplund
-    1.37,  # Juliana Miller
+    1.25,  # Islam Makhachev
+    1.36,  # Mackenzie Dern
+    3.51,  # Charles Johnson
+    1.16,  # Mansur Abdul-Malik
+    3.98,  # Edson Barboza
+    2.74,  # Chidi Njokuani
+    1.57,  # Jalin Turner
+    1.41,  # Donte Johnson
+    2.14,  # Vicente Luque
+    1.83,  # Neil Magny
+    4.94,  # Jeremiah Wells
 ]
 
 odds_fighters2 = [
-    1.66,  # Quillan Salkilld
-    2.27,  # Billy Quarantillo
-    1.13,  # Yadier del Valle
-    1.37,  # Alexia Thainara
-    3.22,  # Billy Ray Goff
-    2.18,  # Bruno Lopes
-    2.81,  # Guilherme Pat
-    2.78,  # Ravena Oliveira
+    3.47,  # Ian Machado Garry
+    2.84,  # Gillian Robertson
+    1.25,  # Jose Ochoa
+    4.58,  # Dustin Stoltzfus
+    1.20,  # Esteban Ribovics
+    1.38,  # Joel Alvarez
+    2.26,  # Kaue Fernandes
+    2.63,  # Eric McConico
+    1.64,  # Tresean Gore
+    1.89,  # Ramiz Brahimaj
+    1.13,  # Myktybek Orolbay
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1,
-      -1]
+      -1, -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
