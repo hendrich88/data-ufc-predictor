c60b2668589babfe787b1f2a7750cb90998eca6e
hendrich88 <hendrich88@gmail.com>
2026-08-16 12:41:10 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,45 +3,43 @@ edge = -666
 min_winner_fights = 0
 min_loser_fights = 0
 
-event_date = "2026-08-15"
-event = "UFC 330: Makhachev vs. Machado Garry"
+event_date = "2026-08-22"
+event = "UFC Fight Night: Hernandez vs. Rodrigues"
 
-event_fighters1 = ["Islam Makhachev", "Mackenzie Dern", "Charles Johnson",  "Mansur Abdul-Malik", "Edson Barboza",  "Chidi Njokuani", "Jalin Turner",
-                   "Donte Johnson", "Vicente Luque", "Neil Magny", "Jeremiah Wells"] 
+event_fighters1 = ["Anthony Hernandez", "Serghei Spivac", "Reinier de Ridder",  "MarQuel Mederos", "Carli Judice",  "Kennedy Nzechukwu", "Wes Schultz",
+                   "Chris Padilla", "Jamall Emmers", "Shanelle Dyer"] 
 
-event_fighters2 =   ["Ian Machado Garry", "Gillian Robertson", "Jose Ochoa",  "Dustin Stoltzfus", "Esteban Ribovics",  "Joel Alvarez", "Kaue Fernandes",
-                     "Eric McConico", "Tresean Gore", "Ramiz Brahimaj",  "Myktybek Orolbai"] 
+event_fighters2 =   ["Gregory Rodrigues", "Vitor Petrino", "Roman Dolidze",  "Mason Jones", "Jeisla Chaves",  "Shamil Gaziev", "Jackson McVey",
+                   "Nasrat Haqparast", "Lerryan Douglas", "Elise Reed"] 
 
 odds_fighters1 = [
-    1.25,  # Islam Makhachev
-    1.36,  # Mackenzie Dern
-    3.51,  # Charles Johnson
-    1.16,  # Mansur Abdul-Malik
-    3.98,  # Edson Barboza
-    2.74,  # Chidi Njokuani
-    1.57,  # Jalin Turner
-    1.41,  # Donte Johnson
-    2.14,  # Vicente Luque
-    1.83,  # Neil Magny
-    4.94,  # Jeremiah Wells
+    1.54,  # Anthony Hernandez
+    2.26,  # Serghei Spivac
+    1.25,  # Reinier de Ridder
+    2.91,  # MarQuel Mederos
+    1.19,  # Carli Judice
+    2.06,  # Kennedy Nzechukwu
+    2.35,  # Wes Schultz
+    1.92,  # Chris Padilla
+    3.53,  # Jamall Emmers
+    1.14,  # Shanelle Dyer
 ]
 
 odds_fighters2 = [
-    3.47,  # Ian Machado Garry
-    2.84,  # Gillian Robertson
-    1.25,  # Jose Ochoa
-    4.58,  # Dustin Stoltzfus
-    1.20,  # Esteban Ribovics
-    1.38,  # Joel Alvarez
-    2.26,  # Kaue Fernandes
-    2.63,  # Eric McConico
-    1.64,  # Tresean Gore
-    1.89,  # Ramiz Brahimaj
-    1.13,  # Myktybek Orolbay
+    2.31,  # Gregory Rodrigues
+    1.57,  # Vitor Petrino
+    3.44,  # Roman Dolidze
+    1.34,  # Mason Jones
+    4.14,  # Jeisla Chaves
+    1.69,  # Shamil Gaziev
+    1.52,  # Jackson McVey
+    1.80,  # Nasrat Haqparast
+    1.24,  # Lerryan Douglas
+    4.88,  # Elise Reed
 ]
 
 hit = [-1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1]
+      -1, -1, -1]
 
 event_accuracy = 0
 event_roi = 0
